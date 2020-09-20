ba2716df2704b98866c24dacf38da9ef83d113a2
namhoang1594 <hnn.nam01@gmail.com>
2020-09-20 23:24:39 +0700

    Update

---
+++ Fan/Fan.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fan
+{
+    class Fan
+    {
+        public const int SLOW = 1;
+        public const int MEDIUM = 2;
+        public const int FAST = 3;
+
+        private int Speed;
+        public void setSpeed(int speed)
+        {
+            Speed = speed;
+        }
+        public int getSpeed()
+        {
+            if (Speed == 2)
+            {
+                return MEDIUM;
+            }
+            else if (Speed == 3)
+            {
+                return FAST;
+            }
+            else
+            {
+                return SLOW;
+            }
+        }
+        private bool on = true;
+        public bool geton()
+        {
+            return on;
+        }
+        public void seton(bool on)
+        {
+            this.on = on;
+        }
+
+        private double Radius = 5;
+        public double getRadius()
+        {
+            return Radius;
+        }
+        public void setRadius(double Radius)
+        {
+            this.Radius = Radius;
+        }
+
+        private string Color = "Blue";
+        public string getColor()
+        {
+            return Color;
+        }
+        public void setColor(string Color)
+        {
+            this.Color = Color;
+        }
+
+        public string ToString()
+        {
+            if (this.on == true)
+            {
+                return $"speed = {this.getSpeed()}, color = {this.Color},radius = {this.Radius}, fan is on";
+            }
+            else if (this.on == false)
+            {
+                return $"color = {this.Color},radius = {this.Radius}, fan is off";
+            }
+            else return "";
+        }
+    }
+}
---
+++ Fan/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Fan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fan 1: Gia tri speed lon nhat, radius 10, color yellow va fan ON");
+            Fan fan_1 = new Fan();
+            fan_1.setSpeed(3);
+            fan_1.setRadius(10);
+            fan_1.setColor("Yellow");
+            Console.WriteLine(fan_1.ToString());
+            Console.WriteLine("\n\n");
+            Console.WriteLine("Fan 2: Gia tri speed trung binh, radius 5, color blue va fan OFF");
+            Fan fan_2 = new Fan();
+            fan_2.setSpeed(2);
+            fan_2.seton(false);
+            Console.WriteLine(fan_2.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
